0883ba85d9ff1a2ac41c08c8990f6a28aaad059d
Franklyn <franklynahilon@gmail.com>
2025-11-05 12:51:45 -0800

    Initial Commit

---
+++ You Are Awesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  You Are Awesome
+//
+//  Created by AHILON CANO, FRANKLYN on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ You Are Awesome/You_Are_AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_AwesomeApp.swift
+//  You Are Awesome
+//
+//  Created by AHILON CANO, FRANKLYN on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
