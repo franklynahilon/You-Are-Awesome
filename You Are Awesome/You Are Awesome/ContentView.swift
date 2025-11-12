ab8ef73b4a04f168f7666f69192eb69496ff36d2
Franklyn <franklynahilon@gmail.com>
2025-11-12 13:22:20 -0800

    1.3 rel

--- You Are Awesome/You Are Awesome/ContentView.swift
+++ You Are Awesome/You Are Awesome/ContentView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I Am A Programmer"
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Spacer()
+            
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                    
+                }
+                Button("Great!") {
+                    message = "Great!"
+                    }
+                }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            }
+            
         .padding()
     }
 }
